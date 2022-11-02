b6965e70a25281e1e17c70ed424634f5c91d0e10
Jahsonte Evans <jahsonteevans@gmail.com>
2022-11-01 22:26:05 -0600

    commit tree 850fee42771a8b6dfcbd15344bd3f25212657255

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+ const cats = ['Milo', 'Otis', 'Garfield']
+    let newCats = [...cats]
+function destructivelyAppendCat() {
+    cats.push('Ralph')
+}
+ 
+function destructivelyPrependCat() {
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat() { 
+    cats.pop('Garfield')
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift('Milo')
+}
+
+function appendCat() {
+    newCats.push('Broom')
+    return newCats
+    
+}
+
+function prependCat() {
+    newCats.unshift('Arnold')
+    newCats.pop('Broom')   
+    return newCats
+}
+
+function removeLastCat() {
+    newCats.pop('Garfield')
+    newCats.shift('Arnold')
+    return newCats
+}
+
+function removeFirstCat() {
+    newCats.shift('Milo')
+    newCats.push('Garfield')
+    return newCats
+}
